7d9488b6fc5dc3af0cfbdaac24212411e58d6c99
LiuKeFeng <liukefeng@bocheninc.com>
2017-12-04 23:16:42 +0800

    add task activity

---
+++ app/src/main/java/com/ifengxue/android/todolist/activity/TaskActivity.java
@@ -0,0 +1,14 @@
+package com.ifengxue.android.todolist.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.ifengxue.android.todolist.R;
+
+public class TaskActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_task);
+  }
+}
